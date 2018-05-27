ddcb86bfe58395817fddc391668dbb7e3d866098
Jason Mardorf <jason.mardorf@hotmail.com>
2018-05-27 16:53:43 -0500

    1.2

---
+++ Substation Builder/View/Transformer2View.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Substation_Builder.View
+{
+    /// <summary>
+    /// Interaction logic for Transformer2View.xaml
+    /// </summary>
+    public partial class Transformer2View : Page
+    {
+        public Transformer2View()
+        {
+            InitializeComponent();
+        }
+    }
+}
