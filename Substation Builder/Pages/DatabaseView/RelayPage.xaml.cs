e47e9652168da8df31687a6b5e6238d5b2d3b5bf
Jason Mardorf <jason.mardorf@hotmail.com>
2019-10-21 17:19:36 -0500

    Dynamic CTs

--- Substation Builder/Pages/DatabaseView/RelayPage.xaml.cs
+++ Substation Builder/Pages/DatabaseView/RelayPage.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Input;
 
 namespace Substation_Builder.Pages.DatabaseView
 {
@@ -10,6 +13,18 @@ namespace Substation_Builder.Pages.DatabaseView
         public RelayPage()
         {
             InitializeComponent();
+        }
+
+        private void TextBox_KeyEnterUpdate(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                TextBox tBox = (TextBox)sender;
+                DependencyProperty prop = TextBox.TextProperty;
+
+                BindingExpression binding = BindingOperations.GetBindingExpression(tBox, prop);
+                if (binding != null) { binding.UpdateSource(); }
+            }
         }
     }
 }
